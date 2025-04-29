249358f0295c6d6203072ac2b820e4255e56cd49
unknown <cuch.aleksander@gmail.com>
2025-04-29 09:09:24 +0200

    new banner

--- src/components/banner/SubPageBanner.tsx
+++ src/components/banner/SubPageBanner.tsx
@@ -10,7 +10,7 @@ export const SubPageBanner = () => {
             sx={{ height: { xs: "24vh", md: "calc(100vh - 100px)" } }}
         >
             <Image
-                src="https://firebasestorage.googleapis.com/v0/b/polish-hema-federation.firebasestorage.app/o/banner.jpg?alt=media&token=1f1dffd9-bb98-4e88-8e46-53324347f806"
+                src="https://firebasestorage.googleapis.com/v0/b/polish-hema-federation.firebasestorage.app/o/banner.jpg?alt=media&token=7ac1b107-f9b0-4c0c-b5fd-9b3729c0e181"
                 alt="Example image"
                 fill
                 priority
